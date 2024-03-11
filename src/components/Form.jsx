c7ec84e930359610f26a8f2ea6bb5306d18a2519
mdasif8393 <md.asif.8393@gmail.com>
2024-03-11 12:50:09 +0600

    done reacting to input with state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,33 +2,75 @@
 
 import { useState } from "react";
 
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (answer.toLowerCase() === "dhaka") {
+        resolve();
+      } else {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      }
+    }, 3000);
+  });
+}
+
 /* eslint-disable react/prop-types */
 export default function Form() {
-  // visual states: empty, typing, submitting, success, error
+  // visual states: typing, submitting, success
 
-  // mandatory state
+  // mandatory data state
   const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+
+  // visual state theke paoa final state
+  const [status, setStatus] = useState("typing");
+
   if (status === "success")
     return (
       <>
-        <p>Status: {status}</p>
         <h1>Thats right!</h1>
-        <hr />
       </>
     );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err.message);
+    }
+  };
+
+  // handlers
+  const handleTextChange = (e) => {
+    setError(null);
+    setAnswer(e.target.value);
+  };
+
   return (
     <>
-      <p>Status: {status}</p>
       <h2>City quiz</h2>
       <p>What city is located on two continents?</p>
-      <form>
-        <textarea></textarea>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextChange}
+          disabled={status === "submitting"}
+        ></textarea>
         <br />
-        <button>Submit</button>
-        <p className="Error">Loading...</p>
-        <p className="Error">There was an error</p>
+        <button
+          type="submit"
+          disabled={answer === "" || status === "submitting"}
+        >
+          Submit
+        </button>
+        {status === "submitting" && <p className="Error">Loading...</p>}
+        {error && <p className="Error">{error}</p>}
       </form>
-      <hr />
     </>
   );
 }
