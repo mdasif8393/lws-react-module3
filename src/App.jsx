d9291bcbdfc75ec4eb3a24358a1b2c6e699e6418
mdasif8393 <md.asif.8393@gmail.com>
2024-03-18 18:02:10 +0600

    3.16 Passing Data Deeply with Context - Introduction to Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,25 @@
-import { useImmerReducer } from "use-immer";
-import TaskList from "./TaskList";
-import AddTask from "./components/AddTask";
-import { initialTasks } from "./data/tasks";
-import taskReducer from "./reducers/taskReducer";
+import Heading from "./components/Heading";
+import Section from "./components/Section";
 
 export default function App() {
-  const [tasks, dispatch] = useImmerReducer(taskReducer, initialTasks);
-
-  const getNextId = (data) => {
-    const maxId = data.reduce((prev, current) =>
-      prev && prev.id > current.id ? prev.id : current.id
-    );
-    return maxId + 1;
-  };
-
-  // handlers
-  const handleAddTask = (text) => {
-    dispatch({
-      type: "added",
-      id: getNextId(tasks),
-      text,
-    });
-  };
-
-  const handleChangeTask = (task) => {
-    dispatch({
-      type: "changed",
-      task,
-    });
-  };
-
-  const handleDeleteTask = (taskId) => {
-    dispatch({
-      type: "deleted",
-      id: taskId,
-    });
-  };
-
   return (
-    <>
-      <h1>Prague itinerary</h1>
-      <AddTask onAdd={handleAddTask} />
-      <TaskList
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-        tasks={tasks}
-      />
-    </>
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
   );
 }
